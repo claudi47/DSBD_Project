e14dd7cc04f7d17c7be7e24ac2850ffcd04d8c12
Alessandro Bellia <casildaserrano@yahoo.it>
2022-02-01 03:55:39 +0100

    updated docker compose and submodules
    Signed-off-by: Alessandro Bellia <casildaserrano@yahoo.it>

--- k8s/start_dsbd.bat
+++ k8s/start_dsbd.bat
@@ -4,6 +4,7 @@ call .\prometheus\start_prometheus.bat
 call .\confluent\confluentop.bat
 call .\mongop\install_mongo.bat
 kubectl apply -k .\mongo-dsbd
+kubectl apply -f .\bot\persistentVolumeClaim.yaml
 kubectl apply -f .\bot\deployment.yaml
 kubectl apply -f .\bet-data-service\deployment.yaml
 kubectl apply -f .\user-service\deployment.yaml
